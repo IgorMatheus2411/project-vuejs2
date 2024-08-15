f6699534b0599784600fb4132b6b3af805b78e3b
Igor Occon <occon24@gmail.com>
2024-08-15 09:15:43 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      
+      <v-spacer></v-spacer>
+
+     
+    </v-app-bar>
+
+    <v-main>
+    
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
